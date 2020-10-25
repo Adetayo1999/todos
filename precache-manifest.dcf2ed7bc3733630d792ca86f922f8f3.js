81b00f0ac26b28e2a0fea09357241797f9dc46d5
Adetayo Omotomiwa <adetayoomotomiwa99@gmail.com>
2020-10-25 03:55:10 -0700

    todos

---
+++ precache-manifest.dcf2ed7bc3733630d792ca86f922f8f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49247911d0d0ae39a56bfda6bf9a0126",
+    "url": "/index.html"
+  },
+  {
+    "revision": "667fad25f92b295fa0dc",
+    "url": "/static/css/main.e3c6de37.chunk.css"
+  },
+  {
+    "revision": "f5f42621fdddd826c127",
+    "url": "/static/js/2.c3b270c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c3b270c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "667fad25f92b295fa0dc",
+    "url": "/static/js/main.3dd09d04.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
